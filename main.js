c7a1b44b22109363843257be7a53a19319525685
omar_yahia <omar.y@smartech.sa>
2025-01-18 17:15:41 +0200

    select jozz range

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 const aya_page_map =[7,12,23,31,36,44,55,64,68,76,83,90,95,100,108,112,119,126,133,141,148,152,160,170,176,183,188,193,197,203,209,217,222,226,231,237,240,244,252,255,259,263,266,271,276,281,288,289,293,302,308,315,322,330,338,345,354,363,370,376,384,393,401,408,414,425,433,441,446,450,458,466,473,479,487,493,499,504,507,512,516,519,526,530,537,544,552,558,567,572,579,584,587,594,598,606,614,620,627,633,640,647,655,663,668,671,674,678,682,686,692,700,705,710,714,719,726,733,739,745,751,758,764,772,777,782,789,797,807,816,824,833,841,848,857,862,870,879,883,890,899,907,913,920,926,931,935,940,946,954,965,976,984,991,997,1005,1011,1021,1027,1035,1041,1049,1058,1074,1084,1091,1097,1103,1109,1113,1117,1124,1132,1141,1149,1160,1168,1176,1185,1193,1200,1205,1212,1221,1229,1235,1241,1248,1255,1261,1266,1271,1275,1282,1289,1296,1303,1307,1314,1321,1328,1334,1341,1346,1352,1357,1364,1370,1378,1384,1389,1397,1406,1417,1425,1434,1442,1452,1461,1470,1478,1485,1492,1501,1510,1518,1526,1535,1544,1554,1561,1570,1581,1590,1600,1610,1618,1626,1633,1639,1648,1659,1665,1674,1682,1691,1699,1707,1712,1720,1725,1735,1741,1749,1755,1760,1768,1774,1783,1792,1802,1817,1833,1853,1872,1892,1907,1915,1927,1935,1943,1955,1965,1973,1980,1988,1994,2003,2011,2019,2029,2036,2046,2056,2067,2078,2087,2095,2105,2115,2125,2133,2144,2155,2160,2167,2174,2185,2193,2201,2214,2223,2237,2250,2261,2275,2288,2301,2314,2326,2345,2360,2385,2399,2412,2424,2435,2446,2461,2473,2483,2493,2507,2518,2527,2540,2555,2564,2573,2584,2595,2600,2610,2618,2625,2633,2641,2650,2659,2667,2673,2690,2700,2715,2732,2747,2762,2777,2791,2801,2811,2818,2822,2827,2834,2844,2849,2852,2857,2866,2875,2887,2898,2910,2922,2932,2951,2971,2992,3015,3043,3068,3091,3115,3138,3159,3172,3181,3194,3203,3214,3222,3235,3247,3257,3265,3273,3280,3287,3295,3302,3311,3322,3329,3336,3346,3354,3363,3370,3378,3385,3392,3403,3414,3424,3433,3441,3450,3459,3469,3480,3488,3497,3503,3514,3523,3533,3539,3548,3555,3563,3568,3576,3583,3587,3595,3606,3613,3620,3628,3637,3645,3654,3663,3671,3678,3690,3698,3704,3717,3732,3745,3759,3775,3788,3812,3839,3864,3890,3914,3941,3970,3986,3996,4012,4031,4053,4063,4068,4079,4089,4098,4105,4114,4125,4132,4140,4149,4158,4166,4173,4182,4191,4199,4210,4218,4229,4238,4247,4256,4264,4272,4282,4287,4294,4303,4316,4323,4335,4347,4358,4372,4385,4398,4414,4432,4453,4473,4486,4495,4505,4515,4524,4530,4538,4545,4556,4564,4574,4583,4592,4598,4606,4611,4616,4623,4630,4645,4665,4681,4705,4726,4749,4766,4784,4810,4828,4852,4873,4895,4917,4941,4968,4995,5029,5055,5078,5086,5093,5099,5104,5110,5115,5125,5129,5135,5142,5150,5155,5161,5168,5177,5185,5192,5199,5208,5217,5222,5229,5236,5241,5253,5267,5286,5313,5331,5357,5385,5414,5429,5447,5460,5475,5494,5512,5542,5570,5596,5616,5641,5672,5702,5727,5758,5800,5829,5854,5882,5909,5931,5963,5993,6016,6043,6072,6098,6125,6137,6155,6176,6193,6207,6221,6236];
 
 let i=new URLSearchParams(window.location.search).get('i');
-const mainAyaId = i?i: newAyaId();
+let min_jozz_id=localStorage.getItem('min_jozz_id')?localStorage.getItem('min_jozz_id'):27;
+let max_jozz_id=localStorage.getItem('max_jozz_id')?localStorage.getItem('max_jozz_id'):30;
+fillJozzRange(min_jozz_id,max_jozz_id);
 
+const min_aya_id=localStorage.getItem('min_aya_id')?parseInt(localStorage.getItem('min_aya_id')):4511;
+const max_aya_id=localStorage.getItem('max_aya_id')?parseInt(localStorage.getItem('max_aya_id')):6236;
+
+const mainAyaId = i?i: newAyaId(min_aya_id, max_aya_id);
 const pageNumber=findPage(mainAyaId,aya_page_map);
 const pagePlacement= pageNumber % 2 == 0 ?'left':'right';
 const pageName='pages/'+pageNumber+'.js?v=0';
@@ -14,10 +20,10 @@ const quranPageImage = document.getElementById('quranPageImage');
 quranPageImage.src = "https://app.quranflash.com/book/MedinaOld/epub/EPUB/imgs/"+String(pageNumber+3).padStart(4, '0')+".png";
 
 function drawRectangles(quranPageImage,ayas) {
-    const imageWidth = quranPageImage.offsetWidth;
-    const imageHeight = quranPageImage.offsetHeight;
-    const naturalWidth = quranPageImage.naturalWidth;
-    const naturalHeight = quranPageImage.naturalHeight;
+    // const imageWidth = quranPageImage.offsetWidth;
+    // const imageHeight = quranPageImage.offsetHeight;
+    // const naturalWidth = quranPageImage.naturalWidth;
+    // const naturalHeight = quranPageImage.naturalHeight;
 
     // Remove existing rectangles to avoid duplication
     canvas.querySelectorAll('.rectangle').forEach(rect => rect.remove());
@@ -83,8 +89,8 @@ function rlQuestionCallBack(){
 function ayaNextPageQuestionCallBack(){
     startRecitingMode();
 }
-function newAyaId(){
-    return getRandomInt(4511,6236);
+function newAyaId(min_aya_id, max_aya_id){
+    return getRandomInt(min_aya_id,max_aya_id);
 }
 function reciteQuestionCallBack(){
     // $('#new_test_link').attr('href',"?i="+newAyaId());
@@ -178,3 +184,59 @@ function findPage(ayaId, lastAyas) {
 
     return  1; //just a fallback to prevent errors
 }
+
+function fillJozzRange(min_jozz_id,max_jozz_id){
+    $('#min_jozz_id').val(min_jozz_id);
+    $('#max_jozz_id').val(max_jozz_id);
+}
+
+function setJozzRange(){
+    let min_jozz_id=parseInt($('#min_jozz_id').val());
+    let max_jozz_id=parseInt($('#max_jozz_id').val());
+    
+    localStorage.setItem('min_jozz_id',min_jozz_id);
+    localStorage.setItem('max_jozz_id',max_jozz_id);
+
+    localStorage.setItem('min_aya_id',jozzs_aya_range[min_jozz_id-1].start_aya_id);
+    localStorage.setItem('max_aya_id',jozzs_aya_range[max_jozz_id-1].end_aya_id);
+    refresh();
+}
+
+function refresh(){
+    window.location.reload();
+}
+
+const jozzs_aya_range=[
+    {"start_aya_id":1,"end_aya_id":148},
+    {"start_aya_id":149,"end_aya_id":259},
+    {"start_aya_id":260,"end_aya_id":384},
+    {"start_aya_id":385,"end_aya_id":516},
+    {"start_aya_id":517,"end_aya_id":640},
+    {"start_aya_id":641,"end_aya_id":750},
+    {"start_aya_id":751,"end_aya_id":899},
+    {"start_aya_id":900,"end_aya_id":1041},
+    {"start_aya_id":1042,"end_aya_id":1200},
+    {"start_aya_id":1201,"end_aya_id":1328},
+    {"start_aya_id":1329,"end_aya_id":1478},
+    {"start_aya_id":1479,"end_aya_id":1648},
+    {"start_aya_id":1649,"end_aya_id":1802},
+    {"start_aya_id":1803,"end_aya_id":2029},
+    {"start_aya_id":2030,"end_aya_id":2214},
+    {"start_aya_id":2215,"end_aya_id":2483},
+    {"start_aya_id":2484,"end_aya_id":2673},
+    {"start_aya_id":2674,"end_aya_id":2875},
+    {"start_aya_id":2876,"end_aya_id":3214},
+    {"start_aya_id":3215,"end_aya_id":3385},
+    {"start_aya_id":3386,"end_aya_id":3563},
+    {"start_aya_id":3564,"end_aya_id":3732},
+    {"start_aya_id":3733,"end_aya_id":4089},
+    {"start_aya_id":4090,"end_aya_id":4264},
+    {"start_aya_id":4265,"end_aya_id":4510},
+    {"start_aya_id":4511,"end_aya_id":4705},
+    {"start_aya_id":4706,"end_aya_id":5104},
+    {"start_aya_id":5105,"end_aya_id":5241},
+    {"start_aya_id":5242,"end_aya_id":5672},
+    {"start_aya_id":5673,"end_aya_id":6236}
+];
+
+
